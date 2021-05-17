a8133bc602635d32f35c6f0731ba168bc2bd9210
yuichiyasui <yassan.b.a.w@gmail.com>
2021-05-17 19:12:39 +0900

    クエリパラメータを返す処理を追加

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,7 +1,9 @@
 class DocumentsController < ApplicationController
   def index
     result = 'indexメソッドが呼ばれました！'
-    puts result
+    if params[:url] then
+      result = params[:url]
+    end
     render json: { result: result}
   end
 end
